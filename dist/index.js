07caf1de1409e8bda0ddf0d187cb86eeb999db09
zhangsen <1226931269@qq.com>
2023-05-05 15:27:36 +0800

    0.1.0

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,30 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.useSingleRequest = exports.SingleRoot = exports.SRProvider = void 0;
-const react_1 = __importDefault(require("react"));
+const react_1 = __importStar(require("react"));
 const react_2 = require("react");
 const recoil_1 = require("recoil");
 const urlMap = new Map();
@@ -43,28 +63,34 @@ const useSingleRequest = (urlkey, request, formater) => {
         return createKey(urlkey);
     }, [createKey, urlkey]);
     const dataAtom = createAtom(datakey, undefined);
-    const loadingAtom = createAtom(loadingKey, false);
+    const loadingAtom = createAtom(loadingKey, true);
     const errorAtom = createAtom(errorKey, false);
     const messageAtom = createAtom(messageKey, "");
     const [data, setData] = (0, recoil_1.useRecoilState)(dataAtom);
     const [isLoading, setLoading] = (0, recoil_1.useRecoilState)(loadingAtom);
     const [isError, setError] = (0, recoil_1.useRecoilState)(errorAtom);
     const [errorMessage, setErrorMessage] = (0, recoil_1.useRecoilState)(messageAtom);
-    if (!urlMap.has(urlkey)) {
-        urlMap.set(urlkey, undefined);
-        request()
-            .then((res) => {
-            const data = formater ? formater(res) : res;
-            setData(data);
-            setLoading(false);
-            urlMap.set(urlkey, data);
-            console.log({ urlMap });
-        })
-            .catch((reason) => {
-            setErrorMessage(reason);
-            setError(true);
-        });
-    }
+    (0, react_1.useEffect)(() => {
+        if (!urlMap.has(urlkey)) {
+            urlMap.set(urlkey, undefined);
+            request()
+                .then((res) => {
+                const data = formater ? formater(res) : res;
+                setData(data);
+                setLoading(false);
+                urlMap.set(urlkey, data);
+            })
+                .catch((reason) => {
+                setErrorMessage(reason);
+                setError(true);
+            });
+        }
+    }, [formater, request, setData, setError, setErrorMessage, setLoading, urlMap, urlkey]);
+    (0, react_1.useEffect)(() => {
+        if (!data && urlMap.has(urlkey)) {
+            setData(urlMap.get(urlkey));
+        }
+    }, [data, setData, urlMap, urlkey]);
     const runRequest = (0, react_2.useCallback)((params) => {
         request(params)
             .then((res) => {
